10216e563bfbc9e0eaee7a054eb3b2dca9bcfd92
Joseph Prachar <josephprachar@comcast.net>
2015-04-14 17:24:38 -0700

    Outlined board and game class

---
+++ GameOfLife/Board.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLife
+{
+    class Board
+    {
+        private int[][] boardOne;
+        private int[][] boardTwo;
+        private byte currentBoard = 0;
+
+        public Board(int[][] startingState)
+        {
+
+        }
+
+        public void Simulate()
+        {
+            // call simulate(x, y) for each cell
+        }
+
+        private void Simulate(int x, int y)
+        {
+            // computes the life or death of the current board via counting surrounding cells
+        }
+
+        private int[] GetSurrounding(int x, int y)
+        {
+            return null;
+        }
+
+        private void Copy(int[][] from, int[][] to)
+        {
+            
+        }
+    }
+}
---
+++ GameOfLife/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace GameOfLife
+{
+    class Game
+    {
+        Board board;
+
+        public Game(int[][] initialState)
+        {
+
+        }
+
+        public void RunOne()
+        {
+            // run one frame of the simulation
+        }
+
+        public void DrawBoard(Graphics g, Point offset)
+        {
+            // draws the board
+        }
+    }
+}
